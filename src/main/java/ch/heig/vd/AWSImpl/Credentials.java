5e1a49e8dbeac7c9670bf9213fe8f49be3a6c029
Janis Chiffelle <janischiffelle@hotmail.ch>
2022-11-01 15:18:18 +0100

    change credentials provider

--- src/main/java/ch/heig/vd/AWSImpl/Credentials.java
+++
@@ -1,7 +0,0 @@
-package ch.heig.vd.AWSImpl;
-
-public class Credentials {
-    public static String id ="id";
-    public static String key = "key";
-    public static String region = "eu-west-2";
-}
