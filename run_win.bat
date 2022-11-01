8591083cee04b824a94468e77823c7c937ffbbb4
Mario Tomic <mario-tomic@hotmail.com>
2022-11-01 16:14:47 +0100

    update readme and gitignore, launch scripts

--- run_win.bat
+++ run_win.bat
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 start java -cp target/Rekognition-1.0-SNAPSHOT.jar ch.heig.vd.App %1
+=======
+start java -cp target/Rekognition-1.0-SNAPSHOT.jar ch.heig.vd.App
+>>>>>>> update readme and gitignore, launch scripts
