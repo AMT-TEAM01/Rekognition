81d9b4ee77cfbfb2f2140fec9bf382696961a29e
Janis Chiffelle <janischiffelle@hotmail.ch>
2022-11-01 14:26:26 +0100

    add aws dependency and test connexion in app

--- src/main/java/ch/heig/vd/App.java
+++ src/main/java/ch/heig/vd/App.java
@@ -1,5 +1,11 @@
 package ch.heig.vd;
 
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Bucket;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Region region = Region.EU_WEST_2;
+        S3Client s3 = S3Client.builder().region(region).build();
+
+        List<Bucket> after = s3.listBuckets().buckets();
+        System.out.println("Your {S3} buckets are:");
+        for (Bucket b : after) {
+            System.out.println("* " + b.name());
+        }
     }
 }
