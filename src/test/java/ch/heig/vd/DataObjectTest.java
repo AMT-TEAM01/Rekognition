f48290371e31c641f83753f923a1db6c089e8e2c
Janis Chiffelle <janischiffelle@hotmail.ch>
2022-11-01 17:59:55 +0100

    wip test upload

--- src/test/java/ch/heig/vd/DataObjectTest.java
+++ src/test/java/ch/heig/vd/DataObjectTest.java
@@ -1,6 +1,7 @@
 package ch.heig.vd;
 
 import ch.heig.vd.AWSImpl.AwsDataObjectHelperImpl;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,6 +9,9 @@ public class DataObjectTest {
 
     AwsDataObjectHelperImpl objImpl;
     private String bucketPath = "amt.team01.diduno.education";
+    private String testName = "test.jpg";
+
+    private String imageUrl = bucketPath + "/" + testName;
 
     @Before
     public void init() {
@@ -17,6 +21,10 @@ public class DataObjectTest {
 
     @Test
     public void testCreateObjectSuccess() {
-        objImpl.
+        Assert.assertTrue(objImpl.bucketExists(bucketPath));
+
+        objImpl.uploadObject(testName, "asd");
+
+        Assert.assertTrue(objImpl.objectExists(bucketPath, testName));
     }
 }
