a205d16e6b1444df0921728189db510cf6f15a66
Janis Chiffelle <janischiffelle@hotmail.ch>
2022-11-01 14:09:14 +0100

    awsCloudClient singleton implementation

--- src/main/java/ch/heig/vd/AWSImpl/AwsCloudClient.java
+++ src/main/java/ch/heig/vd/AWSImpl/AwsCloudClient.java
@@ -1,4 +1,17 @@
 package ch.heig.vd.AWSImpl;
 
 public class AwsCloudClient {
+
+    private static AwsCloudClient instance;
+
+    private AwsCloudClient(){
+
+    }
+
+    public static AwsCloudClient getInstance() {
+        if (instance == null) {
+            instance = new AwsCloudClient();
+        }
+        return instance;
+    }
 }
