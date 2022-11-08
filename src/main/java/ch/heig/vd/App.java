e6782bdd486a0f10b8c647dcb2801f7f0941cbc9
Janis Chiffelle <janischiffelle@hotmail.ch>
2022-11-08 14:15:18 +0100

    update main

--- src/main/java/ch/heig/vd/App.java
+++ src/main/java/ch/heig/vd/App.java
@@ -2,15 +2,18 @@ package ch.heig.vd;
 
 import ch.heig.vd.AWSImpl.AwsCloudClient;
 
+import java.io.IOException;
+
 public class App 
 {
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
         AwsCloudClient client = AwsCloudClient.getInstance();
-        if (args.length == 1) {
-            client.setProfile(args[0]);
-        }
+        client.setProfile(args[0]);
         client.connectHelpers();
+
+        String result = client.execute(args[1], new int[]{200, 90});
+
+        client.uploadObjectWithData("result.json", result);
     }
 }
