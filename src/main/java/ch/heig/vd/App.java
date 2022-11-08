c3772f4ddc846395987ad7c2657853c4b9f6489a
Janis Chiffelle <janischiffelle@hotmail.ch>
2022-11-08 15:05:44 +0100

    finish fix main

--- src/main/java/ch/heig/vd/App.java
+++ src/main/java/ch/heig/vd/App.java
@@ -2,17 +2,30 @@ package ch.heig.vd;
 
 import ch.heig.vd.AWSImpl.AwsCloudClient;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
 
 public class App 
 {
 
+    private static String imageName = "street.jpg";
+    private static String relativePathImages = "/src/main/java/ch/heig/vd/images/";
+
     public static void main( String[] args ) throws IOException {
         AwsCloudClient client = AwsCloudClient.getInstance();
-        client.setProfile(args[0]);
+        if (args.length == 1) {
+            client.setProfile(args[0]);
+        }
         client.connectHelpers();
 
-        String result = client.execute(args[1], new int[]{200, 90});
+        String path = new File("").getAbsolutePath() + relativePathImages;
+        byte[] bytes = Files.readAllBytes(Path.of(path + imageName));
+        String base64 = Base64.getEncoder().encodeToString(bytes);
+
+        String result = client.execute(base64, new int[]{200, 90});
 
         client.uploadObjectWithData("result.json", result);
     }
