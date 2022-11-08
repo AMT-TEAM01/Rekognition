791f035759ca68c11dc635bff9b0b444dfe3c056
Janis Chiffelle <janischiffelle@hotmail.ch>
2022-11-08 15:05:44 +0100

    finish fix main

--- run_win.bat
+++ run_win.bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 start java -cp target/Rekognition-1.0-SNAPSHOT.jar ch.heig.vd.App %1
-=======
-start java -cp target/Rekognition-1.0-SNAPSHOT.jar ch.heig.vd.App
->>>>>>> update readme and gitignore, launch scripts
